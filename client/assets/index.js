b12fa8e7c025c2dcdfb31d33e5977ff1008be2ad
Sally <sallydaveytv@gmail.comgit config --global user.name Sdev77>
2022-06-29 17:25:20 +0100

    first commit

--- client/assets/index.js
+++ client/assets/index.js
@@ -1,15 +1,25 @@
 const dataContainer = document.querySelector(".results-container");
 
-const displayData = function (mov) {
-  mov.forEach((mov, i) => {
-    const html = ` <div class="result-link-container"><a class="result-link" href="${data.address}"></a></div>
-        <div class="result-title-container">
-          <p class="result-desc"><a class="result-link" href="${data.address}"></a></p>${data.link}</p>
-        </div>
-        <div class="result-info-container"><p class="result-info">${data.info}</p></div>`;
+const form = document.querySelector(".btn-test");
+const link = document.querySelector(".link");
+const desc = document.querySelector(".desc");
 
-    dataContainer.insertAdjacentHTML("afterbegin", html);
-  });
-};
+function searchResult(e) {
+  e.preventDefault();
+  fetch("http://localhost:3000/searchresults")
+    .then((res) => res.json())
+    .then((data) => {
+      data.forEach((el, i) => {
+        const html = ` <div class="result-box"> <div class="result-link-container"><a class="result-link" href="${data[i].address}">${data[i].address}</a></div>
+          <div class="result-title-container">
+            <p class="result-desc"><a class="result-link" href="${data[i].address}">${data[i].link}
+            </a></p>
+          </div>
+          <div class="result-info-container"><p class="result-info">${data[i].info}</p></div></div>`;
 
-displayData(data);
+        dataContainer.insertAdjacentHTML("afterbegin", html);
+      });
+    });
+}
+
+form.addEventListener("submit", (e) => searchResult(e));
