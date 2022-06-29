7239b2d787ab3efcba449902a838f3a934f12712
Joshua Warner <joshuawarner51@yahoo.comgit config --global user.name JoshPWarner>
2022-06-29 10:00:42 +0100

    added some html and got the server running

---
+++ server/app.js
@@ -0,0 +1,14 @@
+const express = require('express')
+const app = express()
+const port = 3000;
+const cors = require('cors');
+
+searchResults = [
+]
+
+
+app.get('/', (req,res)=> res.send('Welcome to Google!'))
+
+app.get('/searchresults', (req,res)=>res.send(searchResults))
+
+app.listen(port, () => console.log(`Server is up and running at http://localhost:${port}`))
